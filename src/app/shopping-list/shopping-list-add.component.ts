330e91115b2f9140eb6807cc92dd8b6278f29d03
GEORGE PILITSOLOU <george.pilitsoglou@gmail.com>
2017-12-27 16:34:30 +0000

    Added add ingredient functionality

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ShoppingListService } from './shopping-list.service';
+import { Ingredient } from './../shared/ingredient';
 @Component({
   selector: 'app-shopping-list-add',
   templateUrl: './shopping-list-add.component.html',
   styleUrls: ['./shopping-list-add.component.css']
 })
 export class ShoppingListAddComponent implements OnInit {
+  item: Ingredient;
+  isAdd = true;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(ingredient: Ingredient) {
+    if (!this.isAdd) {
+      // edit
+    } else {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    }
   }
 
 }

675e5eb5889908af1d44c4b9c0c495db6a83bea0
GEORGE PILITSOLOU <george.pilitsoglou@gmail.com>
2017-12-29 21:25:46 +0000

    at header display logged in user

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthenticationService } from './shared/authentication.service';
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor(public auth: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this.auth.login();
+  }
+
+  onLogout() {
+    this.auth.logout();
   }
 
 }

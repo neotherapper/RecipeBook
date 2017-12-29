9692435b83a9fbeb3accbc99e940a984c7e587fe
GEORGE PILITSOLOU <george.pilitsoglou@gmail.com>
2017-12-29 21:24:39 +0000

    added firebase firestore integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { initializeApp } from 'firebase/app';
+
+import { AuthenticationService } from './shared/authentication.service';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -16,6 +23,17 @@ import { routing } from './app.routing';
 import { HomeComponent } from './home/home.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+
+
+  // Initialize Firebase
+  const firebaseConfig = {
+  apiKey: 'AIzaSyBFpt1D6Xae9HHJRrAwI51qkr0VtMdIOsM',
+  authDomain: 'recipebook-e2bb5.firebaseapp.com',
+  databaseURL: 'https://recipebook-e2bb5.firebaseio.com',
+  projectId: 'recipebook-e2bb5',
+  storageBucket: 'recipebook-e2bb5.appspot.com',
+  messagingSenderId: '760536850123'
+};
 
 @NgModule({
   declarations: [
@@ -37,12 +55,16 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     routing
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [
+    AuthenticationService,
     RecipeService,
     ShoppingListService
   ],
